7b53db6fa9348201f458c13bcf6b6e4a6cdfd775
Yangseyeon <seyeon0207@gmail.com>
2024-05-10 14:16:26 +0900

    feat: create project structure
    implement:
    - components structure
    - views structure

--- src/App.vue
+++ src/App.vue
@@ -6,183 +6,23 @@ import '@/assets/js/app.min.js';
 import '@/assets/libs/apexcharts/dist/apexcharts.min.js';
 import '@/assets/libs/simplebar/dist/simplebar.js';
 import { Icon } from '@iconify/vue';
+import Header from "./components/nav/Header.vue";
 </script>
 
 <template>
   <!--  Body Wrapper -->
   <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
     data-sidebar-position="fixed" data-header-position="fixed">
-    <!-- Sidebar Start -->
-    <aside class="left-sidebar">
-      <!-- Sidebar scroll-->
-      <div>
-        <div class="brand-logo d-flex align-items-center justify-content-between">
-          <a href="./index.html" class="text-nowrap logo-img">
-            <img src="@/assets/images/logos/logo.svg" alt="" />
-          </a>
-          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-            <i class="ti ti-x fs-8"></i>
-          </div>
-        </div>
-        <!-- Sidebar navigation-->
-        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-          <ul id="sidebarnav">
-            <li class="nav-small-cap">
-              <Icon icon="solar:menu-dots-linear" class="nav-small-cap-icon fs-4"></Icon>
-              <span class="hide-menu">Home</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./index.html" aria-expanded="false">
-                <Icon icon="solar:widget-add-line-duotone"></Icon>
-                <span class="hide-menu">Dashboard</span>
-              </a>
-            </li>
-            <li>
-              <span class="sidebar-divider lg"></span>
-            </li>
-            <li class="nav-small-cap">
-              <Icon icon="solar:menu-dots-linear" class="nav-small-cap-icon fs-4"></Icon>
-              <span class="hide-menu">UI COMPONENTS</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-buttons.html" aria-expanded="false">
-                <Icon icon="solar:layers-minimalistic-bold-duotone"></Icon>
-                <span class="hide-menu">Buttons</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-alerts.html" aria-expanded="false">
-                <Icon icon="solar:danger-circle-line-duotone"></Icon>
-                <span class="hide-menu">Alerts</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-card.html" aria-expanded="false">
-                <Icon icon="solar:bookmark-square-minimalistic-line-duotone"></Icon>
-                <span class="hide-menu">Card</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-forms.html" aria-expanded="false">
-                <Icon icon="solar:file-text-line-duotone"></Icon>
-                <span class="hide-menu">Forms</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./ui-typography.html" aria-expanded="false">
-                <Icon icon="solar:text-field-focus-line-duotone"></Icon>
-                <span class="hide-menu">Typography</span>
-              </a>
-            </li>
-            <li>
-              <span class="sidebar-divider lg"></span>
-            </li>
-            <li class="nav-small-cap">
-              <Icon icon="solar:menu-dots-linear" class="nav-small-cap-icon fs-4"></Icon>
-              <span class="hide-menu">AUTH</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-                <Icon icon="solar:login-3-line-duotone"></Icon>
-                <span class="hide-menu">Login</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./authentication-register.html" aria-expanded="false">
-                <Icon icon="solar:user-plus-rounded-line-duotone"></Icon>
-                <span class="hide-menu">Register</span>
-              </a>
-            </li>
-            <li>
-              <span class="sidebar-divider lg"></span>
-            </li>
-            <li class="nav-small-cap">
-              <Icon icon="solar:menu-dots-linear" class="nav-small-cap-icon fs-4"></Icon>
-              <span class="hide-menu">EXTRA</span>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./icon-tabler.html" aria-expanded="false">
-                <Icon icon="solar:sticker-smile-circle-2-line-duotone"></Icon>
-                <span class="hide-menu">Icons</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a class="sidebar-link" href="./sample-page.html" aria-expanded="false">
-                <Icon icon="solar:planet-3-line-duotone"></Icon>
-                <span class="hide-menu">Sample Page</span>
-              </a>
-            </li>
-          </ul>
-          <div
-            class="unlimited-access d-flex align-items-center hide-menu bg-primary-subtle position-relative mb-7 mt-4 p-3 rounded">
-            <div class="me-2 flex-shrink-0">
-              <h6 class="fw-semibold fs-4 mb-6 text-dark w-75">Upgrade to pro</h6>
-              <a href="https://adminmart.com/product/matdash-bootstrap-5-admin-dashboard-template/" target="_blank" class="btn btn-primary fs-2 fw-semibold lh-sm">Buy Pro</a>
-            </div>
-            <div class="unlimited-access-img">
-              <img src="@/assets/images/backgrounds/rupee.png" alt="" class="img-fluid">
-            </div>
-          </div>
-        </nav>
-        <!-- End Sidebar navigation -->
-      </div>
-      <!-- End Sidebar scroll-->
-    </aside>
-    <!--  Sidebar End -->
+
     <!--  Main wrapper -->
-    <div class="body-wrapper">
-      <!--  Header Start -->
-      <header class="app-header">
-        <nav class="navbar navbar-expand-lg navbar-light">
-          <ul class="navbar-nav">
-            <li class="nav-item d-block d-xl-none">
-              <a class="nav-link sidebartoggler " id="headerCollapse" href="javascript:void(0)">
-                <i class="ti ti-menu-2"></i>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="javascript:void(0)">
-                <Icon icon="solar:bell-linear" class="fs-6"></Icon>
-                <div class="notification bg-primary rounded-circle"></div>
-              </a>
-            </li>
-          </ul>
-          <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
-            <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-              <a href="https://adminmart.com/product/matdash-free-bootstrap-5-admin-dashboard-template/" target="_blank" class="btn btn-primary">Download Free</a>
-              <li class="nav-item dropdown">
-                <a class="nav-link " href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown"
-                  aria-expanded="false">
-                  <img src="@/assets/images/profile/user-1.jpg" alt="" width="35" height="35" class="rounded-circle">
-                </a>
-                <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
-                  <div class="message-body">
-                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-                      <i class="ti ti-user fs-6"></i>
-                      <p class="mb-0 fs-3">My Profile</p>
-                    </a>
-                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-                      <i class="ti ti-mail fs-6"></i>
-                      <p class="mb-0 fs-3">My Account</p>
-                    </a>
-                    <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-                      <i class="ti ti-list-check fs-6"></i>
-                      <p class="mb-0 fs-3">My Task</p>
-                    </a>
-                    <a href="./authentication-login.html" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-      <!--  Header End -->
+    <div class="body-wrapper ms-0">
+      <Header/>
+
       <div class="body-wrapper-inner">
         <div class="container-fluid">
           <!--  Row 1 -->
           <div class="row">
-            <div class="col-lg-8 d-flex align-items-strech">
+            <div class="col-lg-12 d-flex align-items-strech">
               <div class="card w-100">
                 <div class="card-body">
                   <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
@@ -539,6 +379,6 @@ import { Icon } from '@iconify/vue';
   </div>
 </template>
 
-<style scoped>
+<style>
 @import url('@/assets/css/styles.min.css');
 </style>
