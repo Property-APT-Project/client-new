491127dfa984b4992fcfb6eac62940569459cdde
Yangseyeon <seyeon0207@gmail.com>
2024-05-14 09:42:07 +0900

    feat: add route path

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import MainView from "@/views/MainView.vue";
+import AptView from "@/views/AptView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-  ]
-})
+    {
+      path: "/",
+      name: "root",
+      component: MainView,
+    },
+    {
+      path: "/apt",
+      name: "root",
+      component: AptView,
+    },
+  ],
+});
 
-export default router
+export default router;
