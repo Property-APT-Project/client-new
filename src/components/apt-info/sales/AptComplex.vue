957e8b1336cbb6b8abd3b96c7d09713adea52db6
Yangseyeon <seyeon0207@gmail.com>
2024-05-19 01:35:31 +0900

    fix: no-image

--- src/components/apt-info/sales/AptComplex.vue
+++ src/components/apt-info/sales/AptComplex.vue
@@ -14,9 +14,13 @@ function clickComplexButton(info) {
     <a class="" href="#" @click="clickComplexButton(info)">
       <div class="row bg-gray p-3" style="background-color: white">
         <div class="col-5 d-flex">
-          <img class="rounded w-100 align-middle" :src="info.thumbImg" alt="prd1" />
+          <div class="col-5 d-flex w-100" style="object-fit: cover;">
+            <img v-if="info.thumbImg != '@/assets/images/no-image.jpeg'" class="rounded w-100 align-middle"
+              :src="info.thumbImg" alt="prd1" />
+            <img v-else class="rounded w-100 align-middle" src="@/assets/images/no-image.jpeg" alt="prd1" />
+          </div>
         </div>
-        <div class="col-7">
+        <div class="col-7 m-0">
           <h5 class="omyu_pretty">{{ info.aptName }}</h5>
 
           <h6>{{ info.address }}</h6>
