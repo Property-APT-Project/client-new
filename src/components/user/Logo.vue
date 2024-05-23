ddc4f433553c29ad3d3a77af58f81d2394af3c03
Yangseyeon <seyeon0207@gmail.com>
2024-05-23 15:14:48 +0900

    fix: logo

--- src/components/user/Logo.vue
+++ src/components/user/Logo.vue
@@ -5,7 +5,16 @@
 <template>
   <div>
     <router-link :to="{ name: 'root' }" class="text-nowrap logo-img text-center d-block py-3 w-100">
-      <img src="@/assets/images/logos/logo.svg" alt="">
+      <div>
+          <a href="/" class="text-nowrap logo-img">
+            <div class="d-flex justify-content-center">
+              <img src="@/assets/icons/main-icon.png" height="40px" width="40px" />
+              <h3 class="p-2 d-flex justify-content-center align-items-center"style="font-family: 'yg-jalnan'; color: cornflowerblue; margin-bottom: 0;">이집어때. </h3>
+
+            </div>
+            
+          </a>
+        </div>
     </router-link>
   </div>
 </template>
